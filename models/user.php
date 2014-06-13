3d3c0c7d79715bba7110cf47fa6843c07f2aa347
Luke Denison <ldenison@emich.edu>
2014-06-13 08:51:51 -0400

    added has and belongs to many relationship to models

--- models/user.php
+++ models/user.php
@@ -5,5 +5,14 @@ class User extends Model {
 	public static $table = "RAPTOR_USERS";
 	public static $databaseUser = "ldenison";
 	
+	public $hasAndBelongsToMany = array(
+		"teams" =>
+			array(
+				'className' => 'Team',
+				'joinTable' => 'RAPTOR_TEAMS_USERS',
+				'foreign_key' => 'USER_ID',
+				'association_key' => 'TEAM_ID'
+			)
+	);
 }
 ?>
