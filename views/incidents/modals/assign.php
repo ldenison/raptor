cefdb6040be8eb282caec086b35151d4eedd6298
Luke Denison <ldenison@emich.edu>
2014-06-17 16:29:59 -0400

    added teams to assign incident modal

--- views/incidents/modals/assign.php
+++ views/incidents/modals/assign.php
@@ -1,3 +1,7 @@
+<?php 
+$tc = new TeamsController();
+$teams = $tc->index();
+?>
 <!-- Assign Incident Modal -->
 <div class="modal fade" id="assign-incident" tabindex="-1" role="dialog"
 	aria-labelledby="assign-incident-Label" aria-hidden="true">
@@ -17,6 +21,17 @@
 					<input type="hidden" id="assign_incident_id" name="id" value="4576">
 					<div class="form-group">
 						<input type="text" class="form-control" id="assign_to" name="assign_to" placeholder="Email or Team">
+					</div>
+					<div class="form-group">
+						<label for="team_id">Team</label>
+						<select class="form-control" name="team_id" id="team_id">
+							<option></option>
+							<?php foreach($teams as $t) {?>
+							<option value="<?php echo $t->get("id");?>">
+								<?php echo $t->get("key");?>
+							</option>
+							<?php }?>
+						</select>
 					</div>
 					<div class='form-group'>
 						<textarea class="form-control" name="comment" rows="5" placeholder="Comment..."></textarea>
