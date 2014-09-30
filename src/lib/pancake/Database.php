ad0a6638cb1babd58bdfbed3379bd67e427ac900
Luke Denison <ldenison@emich.edu>
2014-09-30 17:09:45 -0400

    Update Database.php

--- src/lib/pancake/Database.php
+++ src/lib/pancake/Database.php
@@ -11,7 +11,7 @@ class DB extends PDO{
         $this->host = "apptestdb.emich.edu";
         $this->user = "ldenison";
         $this->database = "APPTEST";
-        $this->pass = "h4v3c0k3wb0rt";
+        $this->pass = "********";
         $dns = "oci:dbname=(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = apptestdb.emich.edu)(PORT = 1521)))(CONNECT_DATA=(SID=APPTEST)))";
         parent::__construct($dns,$this->user,$this->pass);
     }
